1c4d477ccd2853dee9ac522c7cda4edbbbeac1dd
Riverine-jpg <nateb2001@gmail.com>
2021-11-06 20:09:15 -0500

    lookup cities finished

--- world.js
+++ world.js
@@ -9,13 +9,14 @@ let url = "http://localhost/info2180-lab5/world.php?country=";
 
     function init(){
         var btn = document.getElementById("lookup");
+        var btnc = document.getElementById("lookupc");
         var outp = document.getElementById("result");
         console.log(btn.value);
         let response;
         let responsearr;
         btn.addEventListener("click",function(event){
             let cname = document.getElementById("country");
-
+            let citieslk = "&context=";
             event.preventDefault();
             xtml.onreadystatechange = function(){
                 if(xtml.readyState = XMLHttpRequest.DONE){
@@ -27,10 +28,31 @@ let url = "http://localhost/info2180-lab5/world.php?country=";
                     }
                 }
             }
-            xtml.open('GET', url + cname.value,false);
-            console.log(url + cname.value);
+            xtml.open('GET', url + cname.value + citieslk,false);
+            console.log(url + cname.value + citieslk);
             xtml.send();
             $("#result").html(response);
         })
+
+        btnc.addEventListener("click",function(event){
+            let cname = document.getElementById("country");
+            let citieslk = "&context=cities";
+            event.preventDefault();
+            xtml.onreadystatechange = function(){
+                if(xtml.readyState = XMLHttpRequest.DONE){
+                    if(xtml.status = 200){
+                        response = xtml.responseText;
+                        
+                    }else{
+                        alert('There was a problem with the request.');
+                    }
+                }
+            }
+            xtml.open('GET', url + cname.value + citieslk,false);
+            console.log(url + cname.value + citieslk);
+            xtml.send();
+            $("#result").html(response);
+        })
+
     }
 })(window,document,undefined)
--- world.php
+++ world.php
@@ -6,30 +6,52 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $cquer = htmlentities($_GET['country'],ENT_QUOTES, 'UTF-8');
+$citquer = htmlentities($_GET['context'],ENT_QUOTES, 'UTF-8');
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$cquer%'");
+if($citquer == "cities"){
+  $stmt = $conn->query("SELECT cities.name,cities.district,cities.population FROM countries INNER JOIN cities ON countries.code=cities.country_code WHERE countries.name LIKE '%$cquer%' ");
+}else{
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$cquer%'");
+}
+
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 
 ?>
-
-
-<table>
-  <tr>
-    <th>Name</th>
-    <th>Continent</th>
-    <th>Independence</th>
-    <th>Head of state</th>
-  </tr>
-  <?php foreach ($results as $row): ?>
+<?php if($citquer == "cities"):?>
+  <table>
     <tr>
-      <td><?= $row['name'] ?></td>
-      <td><?= $row['continent'] ?></td>
-      <td><?= $row['independence_year'] ?></td>
-      <td><?= $row['head_of_state'] ?></td>
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
     </tr>
-  <?php endforeach; ?>
-</table>
+    <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?= $row['name'] ?></td>
+        <td><?= $row['district'] ?></td>
+        <td><?= $row['population'] ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
 
+<?php endif;if($citquer != "cities"):?>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence</th>
+      <th>Head of state</th>
+    </tr>
+    <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?= $row['name'] ?></td>
+        <td><?= $row['continent'] ?></td>
+        <td><?= $row['independence_year'] ?></td>
+        <td><?= $row['head_of_state'] ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+
+<?php endif; ?>
